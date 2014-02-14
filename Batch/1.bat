71cb393175e9d56dd020a58a39b22584c118b6af
Mike Swatek <mike.swatek@synergy55.com>
2014-02-14 14:20:47 -0600

    new file:   ../Batch/1.bat 	new file:   ../Batch/Full.cmd 	new file:   NewTemp.xml

---
+++ Batch/1.bat
@@ -0,0 +1,15 @@
+@echo off
+mode con cols=100 lines=60 >nul
+Color 2
+echo.
+PING 1.1.1.1 -n 2 -w 80.0 >NUL
+echo Have you ever had a dream, Ben, that you were so sure was real?
+echo.
+PING 1.1.1.1 -n 2 -w 100.0 >NUL
+echo What if you were unable to wake from that dream?
+echo.
+PING 1.1.1.1 -n 2 -w 120.0 >NUL
+echo How would you know the difference between the dream world and the real world?
+echo.
+PING 1.1.1.1 -n 2 -w 1500.0 >NUL
+exit
---
+++ Batch/Full.cmd
@@ -0,0 +1,328 @@
+@ECHO OFF
+title Goodbye, Mr. West...
+color 2
+CLS 
+ECHO.
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+CLS 
+ECHO W 
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+
+CLS 
+ECHO Wa 
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+
+CLS 
+ECHO Wak 
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+
+CLS 
+ECHO Wake 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+echo Wake u
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+CLS 
+ECHO Wake up 
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+
+CLS 
+ECHO Wake up, 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Wake up, B 
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+
+CLS 
+ECHO Wake up, Be 
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+
+CLS 
+ECHO Wake up, Ben 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+
+CLS 
+ECHO Wake up, Ben. 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+
+CLS 
+ECHO Wake up, Ben.. 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+
+CLS 
+ECHO Wake up, Ben... 
+ping 10.0.0.1 -n 1 -w 1000 >NUL
+
+CLS 
+ECHO T
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Th
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The M 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Ma
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Mat
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matr 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matri
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix h
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix ha
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix has 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix has y
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix has yo
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix has you
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix has you. 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix has you..
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO The Matrix has you...
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO F
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Fo
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Fol 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Foll 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follo
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow t 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow th
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the w
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the wh
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the whi 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the whit
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white r
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white ra 
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white rab
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white rabb
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white rabbi
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white rabbit
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white rabbit.
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white rabbit..
+ping 10.0.0.1 -n 1 -w 500 >NUL
+
+CLS 
+ECHO Follow the white rabbit...
+ping 10.0.0.1 -n 1 -w 1000 >NUL
+ping 10.0.0.1 -n 1 -w 500 >NUL
+color 07
+CLS
+echo                          \\
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                           \\_
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                        .---(')
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                      o( )_-\_
+ping 10.0.0.1 -n 1 -w 2000 >NUL
+CLS
+echo                               \\
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                \\_
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                             .---(')
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                           o( )_-\_
+ping 10.0.0.1 -n 1 -w 2000 >NUL
+CLS
+echo                                   \\
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                    \\_
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                .---(')
+ping 10.0.0.1 -n 1 -w 500 >NUL 
+echo                              o( )_-\_
+ping 10.0.0.1 -n 1 -w 2000 >NUL
+CLS
+echo                                        \\
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                         \\_
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                     .---(')
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                   o( )_-\_
+ping 10.0.0.1 -n 1 -w 2000 >NUL
+CLS
+echo                                            \\
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                            \\_
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                        .---(')
+ping 10.0.0.1 -n 1 -w 500 >NUL
+echo                                      o( )_-\_
+ping 10.0.0.1 -n 1 -w 2000 >NUL
+CLS
+color 2 
+ECHO Knock, knock, Ben...
+ping 10.0.0.1 -n 1 -w 5000 >NUL
+mode con cols=87 lines=62 >nul
+:start
+color 2
+
+echo                          î                                                       
+ping localhost -n 1 > nul
+echo    ±             ±       ¯                        æ         T     ±                 G
+ping localhost -n 1 > nul 
+echo    ±             ±              æ        ²       ™         H     ±                 O
+ping localhost -n 1 > nul
+echo    ±             ±       œ       ±        ‘       Ÿ         E     ±    ñ            T
+ping localhost -n 1 > nul
+echo    ±             ±       ‘       ê               ‘               ±    ¯            C
+ping localhost -n 1 > nul
+echo    °             °       ê       ‰        ±       á         M     ±    ñ            H
+ping localhost -n 1 > nul
+echo    °         á   °               á       ê       ›        A     °    ¯            A
+ping localhost -n 1 > nul
+echo    °         ‘   °               œ                         T     °    ñ            !
+ping localhost -n 1 > nul
+echo    °         æ   ²     ›         ®   ã            “         R     °                 !
+ping localhost -n 1 > nul
+echo    ²         å   ²     î             “            °         I     °                 !
+ping localhost -n 1 > nul
+echo    ²         ã   ²     ®             ²            ä         X     ²                
+ping localhost -n 1 > nul
+echo    ²         “         ä             ‘                            ²                 H
+ping localhost -n 1 > nul
+echo         œ                     R      œ                      H     ²          ¯      A
+ping localhost -n 1 > nul  
+echo         ƒ                     U      ›                      A     ²          #      H
+ping localhost -n 1 > nul
+echo         ê                     N                            S     ²          ™      A
+ping localhost -n 1 > nul
+echo         æ                                     œ                   ²          #      !     
+ping localhost -n 1 > nul
+echo        æ                     B               á        ±    Y                ‡      !
+ping localhost -n 1 > nul
+echo        æ        Ÿ            E               ‘        ±    O                œ
+ping localhost -n 1 > nul
+echo î                ã            N              °        °    U               
+ping localhost -n 1 > nul
+echo î                ê                       î             °    !               
+ping localhost -n 1 > nul
+echo î                œ            R          ê             ²                            
+ping localhost -n 1 > nul
+echo                  ä            U          ñ             ²                            
+ping localhost -n 1 > nul
+echo                               N          ®                                          
+ping localhost -n 1 > nul                                                                 
+echo                               !                                                   
+ping localhost -n 1 > nul                                                                 
+goto start
+
